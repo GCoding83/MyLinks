938f9d71e49183d307b5574d1ad63f06a23a31df
GCoding83 <girouxcoding@gmail.com>
2019-03-21 16:29:05 -0400

    I was actually able to make the changes to both the left and right sides of the top box. So now, we have the full basic version of the app :)

--- mainapp/static/mainapp/jquery.js
+++ mainapp/static/mainapp/jquery.js
@@ -1,9 +1,11 @@
+/*Handling the right box*/
 $(function(){
 	$('.box-right').on('click', function(){	
 		$(this).next().toggle(100);
 	});
 });
 
+/*Handling the left box*/
 $(function(){
 	$('.box-left').on('click', function(){	
 		var $open = $(this).prev()
@@ -13,6 +15,227 @@ $(function(){
 		} 
 	});
 });
+
+
+
+/*BELOW ARE THE FOUR MAIN FUNCTIONS FOR HANDLING THE TOP BOTTOM BEHAVIORS.*/
+
+/*When user clicks on the bar with the logo, open the top box ("box-top-open")*/
+$(function(){
+	$('.box-top').on('click', function(){	
+		/*Toggle the top box ("box-top-open")*/
+		$(this).next().toggle(100);
+
+		/*Ensure that the top-right and top-left boxes (the one with the citation abstracts and the one with the citation infos) get closed if the top-box (the one with the logo) is closed*/
+		var topRightBox = $(this).siblings('.box-top-right-open');
+		var topLeftBox = $(this).siblings('.box-top-left-open');
+		/*Declare the images that need to be small when user closes the top box*/
+		var anyAbstractImage = $(this).next().find('.top-abstract-image');
+		var anyInfoImage = $(this).next().find('.top-info-image');		
+		/*Declare ANY citation abstracts and authors in this box*/
+		var anyTopRightBoxAuthor = $(this).siblings('.box-top-right-open').find('.top-right-author');
+		var anyTopRightBoxAbstract = $(this).siblings('.box-top-right-open').find('.top-right-abstract');
+		/*Declare ANY citation infos and authors in this box*/
+		var anyTopLeftBoxAuthor = $(this).siblings('.box-top-left-open').find('.top-left-author');
+		var anyTopLeftBoxInfo = $(this).siblings('.box-top-left-open').find('.top-left-info');
+		
+		
+		if ($(topRightBox).is(':visible')){
+/*			close everything, and make the images smaller
+*/			$(anyTopRightBoxAbstract).hide();
+			$(anyTopRightBoxAuthor).hide();
+			$(topRightBox).hide();
+			$(anyAbstractImage).removeClass('image-active');	
+		}
+		if ($(topLeftBox).is(':visible')){
+/*			close everything, and make the images smaller
+*/			$(anyTopLeftBoxInfo).hide();
+			$(anyTopLeftBoxAuthor).hide();
+			$(topLeftBox).hide();
+			$(anyInfoImage).removeClass('image-active');	
+		}
+
+	});
+});
+
+/*When user clicks on the citation in the top box, open the citation description.*/
+$(function(){
+	$('.top-nested-citations').on('click', function(){		
+		/*Declare the box-top-right-open*/
+		var topRightBox = $(this).closest('.box-top-open').siblings('.box-top-right-open');
+		var topLeftBox = $(this).closest('.box-top-open').siblings('.box-top-left-open');
+
+		/*Declare this citation's top-RIGHT id's, i.e. abstract and author id's*/
+		var thisTopRightAbstractId = "topRightAbstract" + $(this).next().attr('id');
+		var thisTopRightAuthorId = "topRightAuhtor" + $(this).next().attr('id');		
+	
+		/*Declare this citation's top-LEFT id's, i.e. info and author id's*/
+		var thisTopLeftInfoId = "topLeftInfo" + $(this).next().attr('id');
+		var thisBottomLeftAuthorId = "topLeftAuhtor" + $(this).next().attr('id');		
+		
+		/*Declare ANY citation abstracts and authors in this box*/
+		var anyTopRightAuthor = $(this).closest('.box-top-open').siblings('.box-top-right-open').find('.top-right-author');
+		var anyTopRightAbstract = $(this).closest('.box-top-open').siblings('.box-top-right-open').find('.top-right-abstract');
+	
+		/*Declare ANY citation info and authors in this box*/
+		var anyTopLeftAuthor = $(this).closest('.box-top-open').siblings('.box-top-left-open').find('.top-left-author');
+		var anyTopLeftInfo = $(this).closest('.box-top-open').siblings('.box-top-left-open').find('.top-left-info');
+	
+		/*The abstract image associated with the citation description*/
+		var thisAbstractImage = $(this).next().find('.top-abstract-image');
+
+		/*The abstract image associated with the citation description*/
+		var thisInfoImage = $(this).next().find('.top-info-image');
+
+		/*if clicked citation abstract is already showing (in the RIGHT box)*/
+		if ($("#"+thisTopRightAbstractId).is(':visible')) {	
+			/*if clicked citation info is also showing (in the LEFT box)*/
+			/*i.e. if BOTH left and right are showing*/
+			if ($("#"+thisTopLeftInfoId).is(':visible')) {	
+				/*close everything, both left and right, and make sure images are small*/			
+				$(anyTopRightAbstract).hide();
+				$(anyTopRightAuthor).hide();
+				$(topRightBox).hide();
+				$(thisAbstractImage).removeClass('image-active');
+				$(anyTopLeftInfo).hide();
+				$(anyTopLeftAuthor).hide();
+				$(topLeftBox).hide();
+				$(thisInfoImage).removeClass('image-active');
+				$(this).next().hide(100);
+			/*if clicked citation abstract alone is showing (in the RIGHT box*/
+			} else {
+				/*Close everything on the right*/
+				$(anyTopRightAbstract).hide();
+				$(anyTopRightAuthor).hide();
+				$(topRightBox).hide();
+				$(thisAbstractImage).removeClass('image-active');
+				$(this).next().hide(100);
+			}
+		/*else if only the clicked citation info is showing (in the LEFT box)*/
+		} else if ($("#"+thisTopLeftInfoId).is(':visible')){
+			/*Close everything on the left*/
+			$(anyTopLeftInfo).hide();
+			$(anyTopLeftAuthor).hide();
+			$(topLeftBox).hide();
+			$(thisInfoImage).removeClass('image-active');
+			$(this).next().hide(100);
+		
+		/*else if neither is visible, on the left or right, then simply toggle the citation description */
+		} else {
+			$(this).next().toggle(100);
+		}
+	});	
+});
+
+
+/*Below is the main function for the TOP-RIGHT (i.e. abstract) box*/
+/*When user clicks on the abstract image in the top box, open the top right box.*/
+$(function(){
+	/*When user clicks on a the abstract image of a publication*/
+	$('body').on('click', '.top-abstract-image', function(){
+		/*Declare the variables that store the unique id'd of each author and abstract of the clicked description */
+		var thisAuthorId = "topRightAuthor" + $(this).closest('.citation-description').attr('id');
+		var thisAbstractId = "topRightAbstract" + $(this).closest('.citation-description').attr('id');
+		/*Declare ANY citation abstracts and authors in this box*/
+		var anyBoxAuthor = $(this).closest('.box-top-open').siblings('.box-top-right-open').find('.top-right-author');
+		var anyBoxAbstract = $(this).closest('.box-top-open').siblings('.box-top-right-open').find('.top-right-abstract');
+		/*Declare ANY abstract image in this box*/
+		var anyAbstractImage = $(this).closest('.box-top-open').find('.top-abstract-image');
+		/*Declare the main box-top-right-open box*/
+		var box = $(this).closest('.box-top-open').siblings('.box-top-right-open');
+		
+
+
+		/*if clicked citation abstract is not showing*/
+		if ($("#"+thisAbstractId).is(':hidden')) {	
+/*			if there already is another citation abstract showing (meaning the box is open)		
+*/			if (anyBoxAbstract.is(':visible')) {
+				/*Make all abstract images regular size and hide the currently showing abstract (and related author)*/
+				$(anyAbstractImage).removeClass('image-active');
+				$(anyBoxAbstract).hide();
+				$(anyBoxAuthor).hide();
+
+				/*Make this abstract image bigger and show the clicked abstract and author*/
+				$(this).addClass('image-active');
+				$("#"+thisAuthorId).show();
+				$("#"+thisAbstractId).show();
+/*			else if no other citation abstract is showing (meaning the box is closed)
+*/			} else {
+				/*Make this abstract image bigger and show the clicked citation abstract (and author) and open the box*/
+				$(this).addClass('image-active');
+				$("#"+thisAuthorId).show();
+				$("#"+thisAbstractId).show();
+				$(box).show();
+			}
+/*		else if clicked citation abstract is already showing (meaning you want to close the box)
+*/		} else {
+/*			close everything
+*/			$(this).removeClass('image-active');
+			$("#"+thisAuthorId).hide();
+			$("#"+thisAbstractId).hide();
+			$(box).hide();
+		}
+	});
+});
+
+
+
+/*Below is the main function for the TOP-LEFT (i.e. info) box*/
+/*When user clicks on the info image in the top box, open the top right box.*/
+$(function(){
+	/*When user clicks on a citation-description of a publication*/
+	$('body').on('click', '.top-info-image', function(){
+		/*Declare the variables that store the unique id'd of each author and info of the clicked description */
+		var thisAuthorId = "topLeftAuthor" + $(this).closest('.citation-description').attr('id');
+		var thisInfoId = "topLeftInfo" + $(this).closest('.citation-description').attr('id');
+		/*Declare ANY citation info and authors in this box*/
+		var anyBoxAuthor = $(this).closest('.box-top-open').siblings('.box-top-left-open').find('.top-left-author');
+		var anyBoxInfo = $(this).closest('.box-top-open').siblings('.box-top-left-open').find('.top-left-info');
+		/*Declare ANY info image in this box*/
+		var anyInfoImage = $(this).closest('.box-top-open').find('.top-info-image');
+		/*Declare the main box-top-right-open box*/
+		var box = $(this).closest('.box-top-open').siblings('.box-top-left-open');
+		
+		/*if clicked citation info is not showing*/
+		if ($("#"+thisInfoId).is(':hidden')) {	
+/*			if there already is another citation info showing (meaning the box is open)		
+*/			if (anyBoxInfo.is(':visible')) {
+				/*Make all abstract images regular size and hide the currently showing info (and related author)*/
+				$(anyInfoImage).removeClass('image-active');
+				$(anyBoxInfo).hide();
+				$(anyBoxAuthor).hide();
+
+				/*Make this info image bigger and show the clicked abstract and author*/
+				$(this).addClass('image-active');
+				$("#"+thisAuthorId).show();
+				$("#"+thisInfoId).show();
+/*			else if no other citation info is showing (meaning the box is closed)
+*/			} else {
+				/*Make this info image bigger and show the clicked citation info (and author) and open the box*/
+				$(this).addClass('image-active');
+				$("#"+thisAuthorId).show();
+				$("#"+thisInfoId).show();
+				$(box).show();
+			}
+/*		else if clicked citation info is already showing (meaning you want to close the box)
+*/		} else {
+/*			close everything
+*/			$(this).removeClass('image-active');
+			$("#"+thisAuthorId).hide();
+			$("#"+thisInfoId).hide();
+			$(box).hide();
+		}
+	});
+});
+
+
+
+
+
+
+
+/*BELOW ARE THE FOUR MAIN FUNCTIONS FOR HANDLING THE BOX BOTTOM BEHAVIORS.*/
+/*The code is not-DRY for now; it repeats the stuff found in the top.*/
 
 /*When user clicks on the bar with the logo, open the bottom box ("box-bottom-open")*/
 $(function(){
@@ -52,16 +275,10 @@ $(function(){
 	});
 });
 
-$(function(){
-	$('.box-top').on('click', function(){	
-		$(this).prev().toggle(100);
-	});
-});
-
 
 /*When user clicks on the citation in the bottom box, open the citation description.*/
 $(function(){
-	$('.nested-citations').on('click', function(){		
+	$('.bottom-nested-citations').on('click', function(){		
 		/*Declare the box-bottom-right-open*/
 		var bottomRightBox = $(this).closest('.box-bottom-open').siblings('.box-bottom-right-open');
 		var bottomLeftBox = $(this).closest('.box-bottom-open').siblings('.box-bottom-left-open');
